a5570be1e23942713fefb3be3a8ee3f2da03e8cb
Batozhab <gorod3012@gmail.com>
2020-06-18 23:58:02 +0800

    fixed

---
+++ MedExpertClientClone/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MedExpertClientClone.Models
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        public string FullName { get; set; }
+        private bool isChecked;
+
+        public bool IsChecked
+        {
+            get
+            {
+                return isChecked;
+            }
+            set
+            {
+                if (value != this.isChecked)
+                {
+                    this.isChecked = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
--- MedExpertClientClone/ViewModels/EmployeeListViewModel.cs
+++ MedExpertClientClone/ViewModels/EmployeeListViewModel.cs
@@ -1,16 +1,44 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using MedExpertClientClone.Models;
 using Rg.Plugins.Popup.Services;
 using Xamarin.Forms;
+using System.Linq;
+
 
 namespace MedExpertClientClone.ViewModels
 {
-    public class EmployeeListViewModel
+    public class EmployeeListViewModel : INotifyPropertyChanged
     {
         public INavigation Navigation { get; set; }
 
+        private ObservableCollection<Employee> employees =
+           new ObservableCollection<Employee>();
+
+        public ObservableCollection<Employee> Employees
+        {
+            get { return employees; }
+            set
+            {
+                employees = value;
+                OnPropertyChanged(nameof(Employees));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public EmployeeListViewModel()
         {
+            Employees = new ObservableCollection<Employee>()
+            {
+                new Employee(){FullName = "Батожаб Будаев"},
+                new Employee(){ FullName = "Раднаев Жамбал"},
+                new Employee(){FullName = "Будаев Будажаб"},
+                new Employee(){ FullName = "Потапов Александр"},
+            };
         }
 
         /// <summary>
@@ -20,5 +48,26 @@ namespace MedExpertClientClone.ViewModels
         {
             await Navigation.PopModalAsync();
         });
+
+        /// <summary>
+        /// Команда закрытия страницы
+        /// </summary>
+        public ICommand ClickCheckBoxCommand => new Command(async (emp) =>
+        {
+            if(emp is Employee employee)
+            {
+                var item = Employees.FirstOrDefault(i => i.FullName == employee.FullName);
+                if (item != null)
+                {
+                    item.IsChecked = !item.IsChecked;
+                }
+            }
+            OnPropertyChanged(nameof(Employees));
+        });
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
