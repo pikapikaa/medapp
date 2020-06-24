0b722db78a4ad07ce172f492850a67e07a59e2f7
Batozhab <gorod3012@gmail.com>
2020-06-24 21:36:56 +0800

    fix

---
+++ MedExpertClientClone.iOS/EntrySearchRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MedExpertClientClone.iOS;
+using MedExpertClientClone.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntryRenderer), typeof(EntrySearchRenderer))]
+namespace MedExpertClientClone.iOS
+{
+    public class EntrySearchRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+            }
+        }
+    }
+}
