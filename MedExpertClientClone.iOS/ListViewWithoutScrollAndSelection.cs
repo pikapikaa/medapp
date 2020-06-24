9aa0764a6ca44ba7de359500215184441a04553d
Batozhab <gorod3012@gmail.com>
2020-06-24 21:36:38 +0800

    fixed

---
+++ MedExpertClientClone.iOS/ListViewWithoutScrollAndSelection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(MedExpertClientClone.iOS.ListViewWithoutScrollAndSelection), nameof(MedExpertClientClone.iOS.ListViewWithoutScrollAndSelection))]
+namespace MedExpertClientClone.iOS
+{
+    public class ListViewWithoutScrollAndSelection : PlatformEffect
+    {
+        private UITableView uITableView => Control as UITableView;
+
+        protected override void OnAttached()
+        {
+            try
+            {
+                if (uITableView != null)
+                {
+                    uITableView.ScrollEnabled = false;
+                    uITableView.AllowsSelection = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        { }
+    }
+}
