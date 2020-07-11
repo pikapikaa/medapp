8f9f489bd16990ab2aaf6f71ab5048c115ff3eaa
admin <gorod3012@gmail.com>
2020-07-11 16:07:09 +0800

    fixed

---
+++ MedExpertClientClone/Views/Popups/MenuAuditListPopupView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using MedExpertClientClone.ViewModels;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace MedExpertClientClone.Views.Popups
+{
+    public partial class MenuAuditListPopupView : PopupPage
+    {
+        public MenuAuditListPopupView()
+        {
+            InitializeComponent();
+            this.BindingContext = new MenuAuditListPopupViewModel();
+        }
+    }
+}
