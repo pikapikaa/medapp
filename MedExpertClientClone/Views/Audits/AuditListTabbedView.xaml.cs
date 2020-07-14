35a9692472ba9c6da9b6c1d0230ec3cef40baf1c
admin <gorod3012@gmail.com>
2020-07-14 21:12:44 +0800

    fixed

---
+++ MedExpertClientClone/Views/Audits/AuditListTabbedView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MedExpertClientClone.ViewModels;
+using Xamarin.Forms;
+
+namespace MedExpertClientClone.Views.Audits
+{
+    public partial class AuditListTabbedView : ContentPage
+    {
+        public AuditListTabbedView()
+        {
+            InitializeComponent();
+            this.BindingContext = new AuditListTabbedViewModel();
+        }
+    }
+}
---
+++ MedExpertClientClone/Views/Audits/AuditListWithDateTabbedView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MedExpertClientClone.Views.Audits
+{
+    public partial class AuditListWithDateTabbedView : ContentPage
+    {
+        public AuditListWithDateTabbedView()
+        {
+            InitializeComponent();
+        }
+    }
+}
